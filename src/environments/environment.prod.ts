729c9e9b48f98acc71b9a64737e1dfa680dab9ee
orlando <orlandoneto212@gmail.com>
2022-10-06 01:23:12 -0300

    alteracao environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'ec2-3-12-152-137.us-east-2.compute.amazonaws.com/',
+  apiUrl: 'http://ec2-3-12-152-137.us-east-2.compute.amazonaws.com/'
 };
