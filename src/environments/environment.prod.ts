f7e6a841c81c7dd0a2522416a844e4720dcea299
orlando <orlandoneto212@gmail.com>
2022-10-07 19:31:37 -0300

    apontamento para back da aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://ec2-3-12-152-137.us-east-2.compute.amazonaws.com/'
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://ec2-3-12-152-137.us-east-2.compute.amazonaws.com/'
 };
