91af71d009559871b804797a32cceeddbc3d8900
orlando <orlandoneto212@gmail.com>
2022-10-07 19:29:11 -0300

    ajuste no redirecionamento netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-3-12-152-137.us-east-2.compute.amazonaws.com/'
+  // apiUrl: 'http://ec2-3-12-152-137.us-east-2.compute.amazonaws.com/'
+  apiUrl: 'http://localhost:8080/'
 };
